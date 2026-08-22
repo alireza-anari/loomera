3944347e094f476ef7c22b2658269898c136feb3
Alireza Anari <111903956+alireza-anari@users.noreply.github.com>
2026-08-22 21:17:53 +0330

    Update liara_pre_start.sh

--- liara_pre_start.sh
+++ liara_pre_start.sh
@@ -7,18 +7,7 @@ python manage.py check --deploy
 python manage.py infrastructure_preflight_check
 python manage.py pre_beta_check
 
-echo "========================================"
-echo "Migration plan..."
-echo "========================================"
-
-PYTHONUNBUFFERED=1 python manage.py migrate --plan --verbosity 2
-
-echo "========================================"
 echo "Running migrations..."
-echo "========================================"
+PYTHONUNBUFFERED=1 python manage.py migrate --noinput
 
-PYTHONUNBUFFERED=1 python manage.py migrate --noinput --verbosity 3
-
-echo "========================================"
 echo "Loomera pre-start finished."
-echo "========================================"
