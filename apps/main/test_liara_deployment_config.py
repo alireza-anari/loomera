01a18ee1bd09f144fea5834d1c92aeef239cc1a0
Alireza Anari <111903956+alireza-anari@users.noreply.github.com>
2026-09-17 02:22:56 +0330

    Update test_liara_deployment_config.py
    change localhost address

--- apps/main/test_liara_deployment_config.py
+++ apps/main/test_liara_deployment_config.py
@@ -21,7 +21,7 @@ class LiaraDeploymentConfigTests(SimpleTestCase):
             command,
         )
         self.assertIn(
-            "http://localhost:8000/health/?live=1",
+            "http://127.0.0.1:8000/health/?live=1",
             command,
         )
 
